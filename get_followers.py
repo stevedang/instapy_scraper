bb7a06d9042658637539775d352143712f1279c9
stevedang <nguyendd@gmail.com>
2019-01-31 14:43:04 +0000

    start with new account nguyendd

--- get_followers.py
+++ get_followers.py
@@ -5,8 +5,8 @@ from instapy import InstaPy
 from instapy.util import smart_run
 
 # login credentials
-insta_username = 'stevednd'
-insta_password = 'yyX^QFP9na7*&Y&q'
+insta_username = 'steve.dang.050'
+insta_password = 'lavina123'
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
@@ -19,8 +19,11 @@ session = InstaPy(username=insta_username,
 with smart_run(session):
     """ Activity flow """
     #account = "urlaubsguru"
-    list1 = ('abindenurlaub', 'reiseuhu', 'travador','globaltraveler.tv' )
+    #list1 = ('abindenurlaub', 'reiseuhu', 'travador','globaltraveler.tv' )
     #list1 = ('airberlin', 'condorairlines', 'tuideutschland')
+    list1 = ('capital_bra')
+    #list1 = ('heuteshow', 'tagesschau', 'gzsz' )
+    #list1 = ('extra3', 'szmagazin', 'bild', 'spiegelonline' )
     for account in list1:
         all_followers = session.grab_followers(
             username=account, amount="full", live_match="False", store_locally= False)
