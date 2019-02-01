0dae1c59075ac58ef3d926f0cb8cb1e1fc172fde
Steve Dang <nguyendd@gmail.com>
2019-02-01 16:25:57 +0800

    get user/pwd and target list from source files

--- get_followers.py
+++ get_followers.py
@@ -3,10 +3,18 @@
 # imports
 from instapy import InstaPy
 from instapy.util import smart_run
+import sys
 
 # login credentials
-insta_username = 'stevednd'
-insta_password = 'yyX^QFP9na7*&Y&q'
+insta_username = ''
+insta_password = ''
+with open("logs/working_user.lst") as input_file:
+    line = input_file.readline().strip()
+    insta_username = line.split(':')[0]
+    insta_password = line.split(':')[1]
+    print("username :"+insta_username)
+    print("password :"+insta_password)
+    sys.stdout.flush()
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
@@ -18,10 +26,9 @@ session = InstaPy(username=insta_username,
 
 with smart_run(session):
     """ Activity flow """
-    #account = "urlaubsguru"
-    list1 = ['abindenurlaub', 'reiseuhu', 'travador','globaltraveler.tv']
-    #list1 = ['airberlin', 'condorairlines', 'tuideutschland']
-    list1 = ['stevednd']
-    for account in list1:
-        all_followers = session.grab_followers(
-            username=account, amount="full", live_match="False", store_locally= False)
+    with open("logs/target_list.lst", 'r') as input_file:
+        for line in input_file:
+            account = line.strip()
+            if account[0] != '#':
+                all_followers = session.grab_followers(
+                    username=account, amount="full", live_match="False", store_locally= False)
